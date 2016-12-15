22c1f100b78d74f6dd8838ba7ba492aa44175434
victor <victorfelipe1608@gmail.com>
2016-12-14 23:35:25 -0200

    Implementação dos àcotes do Sistema

---
+++ implementacao/GeraCon_Aplicacao/src/br/edu/ifnmg/psc/Aplicacao/Entidade.java
@@ -0,0 +1,16 @@
+package br.edu.ifnmg.psc.Aplicacao;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author victor
+ */
+public interface Entidade {
+    public int getId();
+    public void setId(int id);
+}
---
+++ implementacao/GeraCon_Apresentacao/src/br/edu/ifnmg/Apresentacao/GeraCon_Apresentacao.java
@@ -0,0 +1,9 @@
+package br.edu.ifnmg.Apresentacao;
+
+public class GeraCon_Apresentacao {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
