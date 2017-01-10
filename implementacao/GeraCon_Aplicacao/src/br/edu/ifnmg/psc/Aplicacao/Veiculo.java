56afdc1f4efc7c35b8ad7de2338eac5f9e40f00e
thais <bthais27@gmail.com>
2017-01-10 17:41:41 -0200

    Criação da Entidade Veículo.

---
+++ implementacao/GeraCon_Aplicacao/src/br/edu/ifnmg/psc/Aplicacao/Veiculo.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.psc.Aplicacao;
+
+/**
+ *
+ * @author thais
+ */
+public class Veiculo implements Entidade {
+
+    
+    private String placa;
+    private String tipo;
+    private int ano;
+    private String observacoes;
+
+    
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+    
+    @Override
+    public int getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
