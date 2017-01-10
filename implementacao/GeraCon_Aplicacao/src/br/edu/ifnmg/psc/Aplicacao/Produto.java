4e4a04c1f033fcbe96ac6e7681e5b419f714c6a4
thais <bthais27@gmail.com>
2017-01-10 17:50:50 -0200

    Criação da Entidade Produto.

---
+++ implementacao/GeraCon_Aplicacao/src/br/edu/ifnmg/psc/Aplicacao/Produto.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.psc.Aplicacao;
+
+/**
+ *
+ * @author thais
+ */
+public class Produto implements Entidade{
+
+    private String descricao;
+    private float precoUnitario;
+    private String unidadeMedida;
+    
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(float precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+
+    public void setUnidadeMedida(String unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+    
+    
+    @Override
+    public int getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
