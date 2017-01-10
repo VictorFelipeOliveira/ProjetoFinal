74e0a4bbd52b747181ac61098d446b43efc05908
thais <bthais27@gmail.com>
2017-01-10 17:46:13 -0200

    Criação da Entidade Funcionário.

---
+++ implementacao/GeraCon_Aplicacao/src/br/edu/ifnmg/psc/Aplicacao/Funcionario.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.psc.Aplicacao;
+
+/**
+ *
+ * @author thais
+ */
+public class Funcionario implements Entidade {
+
+    private String carteiraTrabalho;
+    private String cargo;
+    private String cpf;
+
+    public String getCarteiraTrabalho() {
+        return carteiraTrabalho;
+    }
+
+    public void setCarteiraTrabalho(String carteiraTrabalho) {
+        this.carteiraTrabalho = carteiraTrabalho;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    @Override
+    public int getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
