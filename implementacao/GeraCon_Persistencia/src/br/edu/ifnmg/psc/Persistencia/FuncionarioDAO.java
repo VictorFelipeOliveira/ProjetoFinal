9885ea9353a184c9053976f0f479c19352c3b7f6
victor <victorfelipe1608@gmail.com>
2017-01-18 23:59:36 -0200

    Inclusão de entidades - Persistencia

--- implementacao/GeraCon_Persistencia/src/br/edu/ifnmg/psc/Persistencia/FuncionarioDAO.java
+++ implementacao/GeraCon_Persistencia/src/br/edu/ifnmg/psc/Persistencia/FuncionarioDAO.java
@@ -21,11 +21,17 @@ import java.util.logging.Logger;
 public class FuncionarioDAO extends DAOGenerico<Funcionario> implements FuncionarioRepositorio {
 
     public FuncionarioDAO(){
-        setConsultaAbrir(null);
-        setConsultaApagar(null);
-        setConsultaInserir(null);
-        setConsultaBusca(null);
-        setConsultaUltimoId(null);
+         setConsultaAbrir("select codigo, nome, telefone, email, cpf, cargo, "
+                + "carteiraTrabalho, rua, bairro, cidade, complemento from Funcionarios where codigo = ?");
+        setConsultaApagar("delete from Funcionarios where codigo = ?");
+        setConsultaInserir("insert into Funcionarios (nome, telefone, email, cpf, cargo, "
+                + "carteiraTrabalho, rua, bairro, cidade, complemento) values (?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        setConsultaAlterar("update Funcionarios set nome = ?, telefone = ?, email = ?, cpf = ?, "
+                + "cargo = ?, carteiraTrabalho = ?, rua = ?, bairro = ?, cidade = ?, complemento = ? "
+                + "where codigo = ?");
+        setConsultaBusca("select codigo, nome, telefone, email, cpf, cargo, carteiraTrabalho, "
+                + "rua, bairro, cidade, complemento from Funcionarios");
+        setConsultaUltimoId("select max(codigo) from Funcionarios where nome = ?, and cpf = ? and carteiraTrabalho = ?");
     }
 
     @Override
@@ -33,17 +39,17 @@ public class FuncionarioDAO extends DAOGenerico<Funcionario> implements Funciona
         Funcionario funcionario = new Funcionario();
         
        try{
-            funcionario.setId(id);
-            funcionario.setCpf(null);
-            funcionario.setCargo(null);
-            funcionario.setNome(null);
-            funcionario.setTelefone(null);
-            funcionario.setEmail(null);
-            funcionario.setCarteiraTrabalho(null);
-            funcionario.setCidade(null);
-            funcionario.setComplemento(null);
-            funcionario.setRua(null);
-            funcionario.setNumero(0);
+            funcionario.setId(resultado.getInt(1));
+            funcionario.setNome(resultado.getString(2));
+            funcionario.setTelefone(resultado.getString(3));
+            funcionario.setEmail(resultado.getString(4));
+            funcionario.setCpf(resultado.getString(6));
+            funcionario.setCargo(resultado.getString(7));
+            funcionario.setCarteiraTrabalho(resultado.getString(8));
+            funcionario.setRua(resultado.getString(9));
+            funcionario.setBairro(resultado.getString(10));
+            funcionario.setCidade(resultado.getString(11));
+            funcionario.setComplemento(resultado.getString(12));
         
         }catch(SQLException ex){
            ex.printStackTrace();
@@ -54,17 +60,17 @@ public class FuncionarioDAO extends DAOGenerico<Funcionario> implements Funciona
     protected void preencheConsulta(PreparedStatement sql, Funcionario funcionario) {
             
         try {
-            sql.setString(1,  funcionario.getNome() );
-            sql.setString(2,  funcionario.getCpf());
-            sql.setString(3, funcionario.getCarteiraTrabalho());
-            sql.setString(4, funcionario.getCargo());
-            sql.setString(5,  funcionario.getEmail());
-            sql.setString(6,  funcionario.getTelefone());
-            sql.setString(7,  funcionario.getBairro());
-            sql.setString(8,  funcionario.getCidade());
-            sql.setString(9,  funcionario.getComplemento());
-            sql.setString(10, funcionario.getRua());
-            sql.setInt(11, funcionario.getNumero());
+            sql.setString(1,  cliente.getNome() );
+            sql.setString(2,  cliente.getCpf());
+            sql.setString(3,  cliente.getDescricao());
+            sql.setString(4,  cliente.getRg());
+            sql.setString(5,  cliente.getEmail());
+            sql.setString(6,  cliente.getTelefone());
+            sql.setString(7,  cliente.getBairro());
+            sql.setString(8,  cliente.getCidade());
+            sql.setString(9,  cliente.getComplemento());
+            sql.setString(10, cliente.getRua());
+            sql.setInt(11, cliente.getNumero());
         } catch (SQLException ex) {
             Logger.getLogger(FuncionarioDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
---
+++ implementacao/GeraCon_Persistencia/src/br/edu/ifnmg/psc/Persistencia/UsuarioDAO.java
@@ -0,0 +1,108 @@
+package br.edu.ifnmg.psc.Persistencia;
+        
+import br.edu.ifnmg.psc.Aplicacao.Usuario;
+import br.edu.ifnmg.psc.Aplicacao.UsuarioRepositorio;
+import br.edu.ifnmg.psc.Excecao.ErroValidacao;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author victor
+ */
+public class UsuarioDAO extends DAOGenerico<Usuario> implements UsuarioRepositorio{
+
+    public UsuarioDAO() {
+        setConsultaAbrir("select id, login, senha from Usuario where id = ?");
+        setConsultaApagar("delte from usuario where id = ?");
+        setConsultaInserir("insert into usuario(login, senha) values (?, ?, ?)");
+        setConsultaAlterar("update usuario set login = ?, senha = ? where id = ?");
+        setConsultaBusca("select id, login, senha from usuarios");
+        setConsultaUltimoId("select max(id) from usuarios  where login = ? and senha = ?");
+    }
+
+    @Override
+    protected Usuario preencheObjeto(ResultSet resultado) {
+        Usuario usuario = new Usuario();
+        
+        try{
+                usuario.setId(resultado.getInt(1));
+                usuario.setLogin(resultado.getString(2));
+                usuario.setSenha(resultado.getString(3));
+        } catch (SQLException ex) {
+             ex.printStackTrace();
+        }
+        
+        return usuario;
+    }
+
+    @Override
+    protected void preencheConsulta(PreparedStatement sql, Usuario usuario) {
+         try {
+           
+            sql.setString(1, usuario.getLogin());
+            sql.setString(2, usuario.getSenha());
+            
+            //if(usuario.getId() > 0) 
+                //sql.setInt(4,usuario.getId());
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void preencheFiltros(Usuario filtro) {
+        if(filtro.getId() > 0) adicionarFiltro("id", "=");
+        if(filtro.getLogin() != null) adicionarFiltro("login", " like ");
+        if(filtro.getSenha()!= null) adicionarFiltro("senha", "=");
+    }
+
+    @Override
+    protected void preencheParametros(PreparedStatement sql, Usuario filtro) {
+        try {
+            int cont = 1;
+            if(filtro.getId() > 0){ sql.setInt(cont, filtro.getId()); cont++; }
+            if(filtro.getLogin() != null ){ sql.setString(cont, filtro.getLogin() +"%"); cont++; }
+            if(filtro.getSenha() != null){ 
+                sql.setString(cont, filtro.getSenha()); 
+                cont++; 
+            }
+                   
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public Usuario Abrir(String login) {
+        try {
+            
+            PreparedStatement sql = conn.prepareStatement("select id,login,senha from usuarios where login = ?");
+           
+            sql.setString(1,login);
+            
+            ResultSet resultado = sql.executeQuery();
+            
+            if(resultado.next()){
+            
+                return preencheObjeto(resultado);
+            }            
+        }  catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        
+        return null;
+    }
+    
+}
+    
+
+
